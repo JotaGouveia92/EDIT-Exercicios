3edb37d3ac5de748d2b231566efee71b0a3ecb50
Joao Gouveia <joaogouveia.work@gmail.com>
2025-08-19 18:39:52 +0100

    ADD FOLDER

--- JS/exercicio_final/ecommerce.js
+++ JS/exercicio_final/ecommerce.js
@@ -1,127 +1,161 @@
-// # - Criar um ficheiro html com uma tag script;
-// # - Criar uma class chamada Ecommerce;
-// # - Criar os seguintes métodos para a class:
-// # ○ addProduct: aceita um produto e o adicione à lista de produtos;
-// # ○ setProductPrice:
-// # ■ Aceita 2 parametros: productId e price;
-// # ■ Deve actualizar o produto com o preço novo (price).
-// # ○ getAllProducts: devolve a lista de todos os produtos;
-// # ○ getAllProductsNames: devolve uma string com todos os nomes dos produtos, separados por ponto e virgula “;” ;
-// # ○ getProductById: aceita um id de produto e devolve esse produto;
-// # ○ getProductsByPrice:
-// # ■ aceita 2 parametros: initialPrice e finalPrice
-// # ■ Devolve os produtos que sejam iguais ou superiores a initialPrice e menores ou iguais a finalPrice;
-// # ○ addProductToCart: aceita um produto e adiciona esse produto ao carrinho de compras;
-// # ○ getCartTotalPrice: devolve a soma de todos os produtos do carrinho;
-// # - Um produto é composto pelos campos: name, price, id;
-// # - O carrinho de compras é um array de produtos;
-
 class Ecommerce {
-    constructor(){
-        this.products = [];
-        this.cart = [];
+  constructor() {
+    this.products = [];
+    this.cart = [];
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+  }
+
+  setProductPrice(productId, price) {
+    const product = this.products.find((product) => product.id === productId);
+    if (product) {
+      product.price = price;
     }
+  }
 
-    addProduct(product){
-        this.products.push(product);
-    }
+  getAllProducts() {
+    console.warn("List All products");
+    console.table(this.products);
+    return this.products;
+  }
 
-    // setProductPrice(productId, price){ 
-    //     this.products = this.products.map(product => {
-    //         if (product.id !== productId) {
-    //             return product
-    //         } 
-    //         return {
-    //             ...product,
-    //             price
-    //         }
-    //     })
-    // }
+  getAllProductsNames() {
+    const allProductsNames = this.products
+      .map((product) => product.name)
+      .join(";");
+    console.log("All Products Names: " + allProductsNames);
+    return allProductsNames;
+  }
 
-    setProductPrice(productId, price) {
-        const product = this.products.find(product => product.id === productId);
-        if (product) {
-            product.price = price;
-        }
-    }
+  getProductById(productId) {
+    const product = this.products.find((product) => product.id === productId);
+    console.log("All Products ID: " + JSON.stringify(product));
+    return product;
+  }
 
-    getAllProducts(){
-        console.warn("List All products")
-        console.table(this.products);
-        return this.products;
-    }
+  getProductsByPrice(initialPrice, finalPrice) {
+    const productsByPrice = this.products.filter(
+      ({ price }) => price >= initialPrice && price <= finalPrice
+    );
+    console.table(productsByPrice);
+    return productsByPrice;
+  }
 
-    getAllProductsNames(){
-        const allProductsNames = this.products.map(product => product.name).join(";")
-        console.log("All Products Names: " + allProductsNames);
-        return allProductsNames;
-    }
+  addProductToCart(product) {
+    this.cart.push(product);
+  }
 
-    getProductById(productId){
-        const product = this.products.find((product) => product.id === productId )
-        console.log("All Products ID: " + JSON.stringify(product));
-        return product;
-    }
-
-    getProductsByPrice(initialPrice, finalPrice){
-       const productsByPrice = this.products.filter(({price}) => price >= initialPrice && price <= finalPrice)
-       console.table(productsByPrice);
-       return productsByPrice;
-    }
-
-    addProductToCart(product){
-        this.cart.push(product)
-    }
-
-    getCartTotalPrice(){
-        const totalPrice = this.cart.reduce(
-            (accumulator, product) => accumulator + product.price, 0
-        )
-        console.log("Total Price: " + totalPrice);
-        return totalPrice;
-    } 
+  getCartTotalPrice() {
+    const totalPrice = this.cart.reduce(
+      (accumulator, product) => accumulator + product.price,
+      0
+    );
+    console.log("Total Price: " + totalPrice);
+    return totalPrice;
+  }
 }
+
+// shop.addProduct({
+//   id: 50,
+//   name: "t-shirt",
+//   price: 90,
+// });
+
+// shop.addProduct({
+//   id: 39,
+//   name: "shirt",
+//   price: 49,
+// });
+
+// shop.addProduct({
+//   id: 30,
+//   name: "trousers",
+//   price: 50,
+// });
+
+// shop.addProduct({
+//   id: 89,
+//   name: "sweatshirt",
+//   price: 19,
+// });
+
+// shop.addProduct({
+//   id: 10,
+//   name: "shorts",
+//   price: 50,
+// });
+
+// shop.getAllProducts();
+// shop.setProductPrice(30, 60);
+// shop.getAllProducts();
+// shop.getAllProductsNames();
+// shop.getProductById(39);
+// shop.getProductsByPrice(10, 60);
+// shop.getProductsByPrice(80, 100);
+
+// shop.addProductToCart(shop.getProductById(10));
+
+// shop.getCartTotalPrice();
 
 const shop = new Ecommerce();
 
-shop.addProduct({
-    id:50,
-    name:"t-shirt",
-    price:90
-});
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((products) => {
+    const container = document.getElementById("products-container");
+    products.forEach((product) => {
+      const card = document.createElement("div");
+      const title = document.createElement("h2");
+      const imgProduct = document.createElement("img");
+      imgProduct.src = product.image;
+      imgProduct.alt = product.title;
+      const addButton = document.createElement("button");
+      addButton.addEventListener("click", () => {
+        shop.addProductToCart(product);
 
-shop.addProduct({
-    id:39,
-    name:"shirt",
-    price:49
-});
+        const payload = {
+          userId: 1,
+          date: new Date().toISOString().split("T")[0],
+          products: shop.cart.map((products) => ({
+            productId: products.id,
+            quantity: products.quantity,
+          })),
+        };
 
-shop.addProduct({
-    id:30,
-    name:"trousers",
-    price:50
-});
+        fetch("https://fakestoreapi.com/carts/3", {
+          method: "PUT",
+          mode: "cors",
+          cache: "no-cache",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          redirect: "follow",
+          referrerPolicy: "no-referrer",
+          body: JSON.stringify(payload),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Resposta da API", data);
+            alert(`${product.title} adicionado ao carrinho através da API.`);
+          })
+          .catch((error) => {
+            console.error("Erro", error);
+            alert("Erro ao enviar para a API.");
+          });
+      });
 
-shop.addProduct({
-    id:89,
-    name:"sweatshirt",
-    price:19
-});
+      card.append(title);
+      card.append(imgProduct);
+      card.append(addButton);
 
-shop.addProduct({
-    id:10,
-    name:"shorts",
-    price:50
-});
+      container.append(card);
 
-shop.getAllProducts();
-shop.setProductPrice(30, 60);
-shop.getAllProducts();
-shop.getAllProductsNames();
-shop.getProductById(39);
-shop.getProductsByPrice(10,60);
-shop.getProductsByPrice(80,100);
+      // FOOTER
 
-shop.addProductToCart(shop.getProductById(10));
-
-shop.getCartTotalPrice();
+      let footer = document.getElementsByTagName("footer")[0];
+      footer.textContent = `Copyright ${new Date().getFullYear()}`;
+    });
+  });
