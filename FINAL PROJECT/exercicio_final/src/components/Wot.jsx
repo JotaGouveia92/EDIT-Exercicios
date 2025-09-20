770f6cabd7f651f13aec0edbccd55aee57aa3479
Joao Gouveia <joaogouveia.work@gmail.com>
2025-09-20 16:50:06 +0100

    UPDATE REPOSITORY

--- FINAL PROJECT/exercicio_final/src/components/Wot.jsx
+++
@@ -1,9 +0,0 @@
-export default function Wot() {
-  return (
-    <div>
-      <div className="wot" data-margin-bottom="s">
-        <h2 className="wot__title">What I've been working so far</h2>
-      </div>
-    </div>
-  );
-}
