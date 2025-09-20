688e1117e0e6e781d0d963cbf69badea354704a0
Joao Gouveia <joaogouveia.work@gmail.com>
2025-09-20 16:33:36 +0100

    fixing general issues

--- SASS/exercicio_final/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function () {
-	if (window.pageYOffset >= 200) {
-		document.querySelector('header').classList.add('fixed');
-	} else {
-		document.querySelector('header').classList.remove('fixed');
-	}
-});
